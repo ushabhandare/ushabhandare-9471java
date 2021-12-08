a925f4ec200f5a4233e8b46ce52416430ee79060
USHA BHANDARE <95093338+ushabhandare@users.noreply.github.com>
2021-12-08 11:29:18 +0530

    Create Main.java

---
+++ Icebreaking/2/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        System.out.print("Enter number to find out the cube: ");
+        int i = new Scanner(System.in).nextInt();
+        System.out.println("Number is: "+i+" and cube of "+i+" is: "+i*i*i);
+    }
+}
