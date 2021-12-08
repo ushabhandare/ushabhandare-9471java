706a29636310c1dbc41a9ce7a8bbf8c3126751e2
USHA BHANDARE <95093338+ushabhandare@users.noreply.github.com>
2021-12-08 11:46:20 +0530

    Create SInheritance.java

---
+++ Anudip Practicals/SInheritance.java
@@ -0,0 +1,17 @@
+class Animal{
+void eat(){
+System.out.println("Eating");}
+}
+
+class Dog extends Animal{
+void bark(){
+System.out.println("Barking");}
+
+}
+class SInheritance{
+public static void main(String args[]){
+Dog d = new Dog();
+d.bark();
+d.eat();
+}
+}
