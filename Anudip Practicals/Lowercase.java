012129188977da2342921db1269baf58ac3bf8f2
USHA BHANDARE <95093338+ushabhandare@users.noreply.github.com>
2021-12-08 11:54:41 +0530

    Create Lowercase.java

---
+++ Anudip Practicals/Lowercase.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Lowercase {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a String: ");
+		String line = in.nextLine();
+		line = line.toLowerCase();
+		System.out.println(line);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
